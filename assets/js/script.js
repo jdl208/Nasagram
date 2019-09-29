5688e4aa3b1a6ad043abfa524541c24f037c7681
Ubuntu <ubuntu@ip-172-31-43-105.ec2.internal>
2019-09-29 11:08:04 +0000

    Corrected the way data from the Apod api is shown

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,38 +1,54 @@
-// Set variable to today's date
+$(document).ready(function() {
+    // NAV Buttons to slide in and out the requested section
 
-var d = new Date();
-var month = d.getMonth() + 1;
-var day = d.getDate();
-var today = d.getFullYear() + '-' + (('' + month).length < 2 ? '0' : '') + month + '-' + (('' + day).length < 2 ? '0' : '') + day;
+    $("#apodNav").click(function() {
+        $("#epic").slideUp("slow", function() {
+            $("#mars").slideUp("slow", function() {
+                $("#apod").slideDown("slow")
+            });
+        });
+    });
+    $("#epicNav").click(function() {
+        $("#apod").slideUp("slow", function() {
+            $("#mars").slideUp("slow", function() {
+                $("#epic").slideDown("slow")
+            });
+        });
+    });
+    $("#marsNav").click(function() {
+        $("#apod").slideUp("fast", function() {
+            $("#epic").slideUp("fast", function() {
+                $("#mars").slideDown("slow")
+            });
+        });
+    });
 
-// Astronomy Picture of the Day
+    // Set variable to today's date
 
-function getApod(cb) {
-    let xhr = new XMLHttpRequest();
-
-    xhr.open("GET", "https://api.nasa.gov/planetary/apod?date=" + today + "&api_key=wUAJ4mhB3TmjgWUsGryQlRMw59v3Wzbg3xXc8AlR");
-    xhr.send();
-
-    xhr.onreadystatechange = function() {
-        if (this.readyState == 4 && this.status == 200) {
-            cb(JSON.parse(this.responseText));
-        }
-    };
-}
-
-getApod(function(data) {
+    var d = new Date();
+    var month = d.getMonth() + 1;
+    var day = d.getDate();
+    var today = d.getFullYear() + '-' + (('' + month).length < 2 ? '0' : '') + month + '-' + (('' + day).length < 2 ? '0' : '') + day;
     document.getElementById("apodDate").setAttribute("max", today);
     document.getElementById("apodDate").setAttribute("value", today);
-    document.getElementById("apodDate").innerHTML = data.date;
-    document.getElementById("apodTitle").innerHTML = data.title;
-    document.getElementById("apodImg").src = data.url;
-    document.getElementById("apodExplanation").innerHTML = data.explanation;
-    document.getElementById("exampleModalLabel").innerHTML = data.title;
 
-});
+    // Astronomy Picture of the Day
 
-function getApodFromInput(event) {
-    today = $("#apodDate").val();
+    function getApod(cb) {
+        let xhr = new XMLHttpRequest();
+
+        var apodDate = $("#apodDate").val();
+
+        xhr.open("GET", "https://api.nasa.gov/planetary/apod?date=" + apodDate + "&api_key=wUAJ4mhB3TmjgWUsGryQlRMw59v3Wzbg3xXc8AlR");
+        xhr.send();
+
+        xhr.onreadystatechange = function() {
+            if (this.readyState == 4 && this.status == 200) {
+                cb(JSON.parse(this.responseText));
+            }
+        };
+    }
+
     getApod(function(data) {
         document.getElementById("apodDate").innerHTML = data.date;
         document.getElementById("apodTitle").innerHTML = data.title;
@@ -41,26 +57,17 @@ function getApodFromInput(event) {
         document.getElementById("exampleModalLabel").innerHTML = data.title;
 
     });
-}
 
-$("#apodNav").click(function() {
-    $("#epic").slideUp("slow", function() {
-        $("#mars").slideUp("slow", function() {
-            $("#apod").slideDown("slow")
+    $("#apodDate").change(function(){
+        getApod(function(data) {
+            document.getElementById("apodDate").innerHTML = data.date;
+            document.getElementById("apodTitle").innerHTML = data.title;
+            document.getElementById("apodImg").src = data.url;
+            document.getElementById("apodExplanation").innerHTML = data.explanation;
+            document.getElementById("exampleModalLabel").innerHTML = data.title;
         });
     });
-});
-$("#epicNav").click(function() {
-    $("#apod").slideUp("slow", function() {
-        $("#mars").slideUp("slow", function() {
-            $("#epic").slideDown("slow")
-        });
-    });
-});
-$("#marsNav").click(function() {
-    $("#apod").slideUp("fast", function() {
-        $("#epic").slideUp("fast", function() {
-            $("#mars").slideDown("slow")
-        });
-    });
+
+    // EPIC - Earth Polychromatic Imaging Camera
+
 });
